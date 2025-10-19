b88ff3bcf1028bbfd4c28874092447a065673957
Goodness Ojonuba <99451974+22trix@users.noreply.github.com>
2025-10-19 23:23:51 +0100

    Integrating Db into python

---
+++ MySQLServer.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from mysql.connector import Error
+try:
+    mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Qw@TY123",
+            database="mydb"
+        )
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    mydb.commit()
+    if mydb.is_connected():
+        print("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error:
+    print("Error connecting to the database")
+finally:
+    mycursor.close()
+    mydb.close()
